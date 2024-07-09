86d84a23daf445607c0bad022de827560ebaeb92
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-07-08 18:33:21 -0700

    Set up email form and "coming soon" overlay

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,3 +7,17 @@ function goToElement(element) {
     }
   );
 }
+
+function revealEmailForm() {
+  const emailFormContainer = document.getElementById("email-form-container"); 
+  const maxHeight = emailFormContainer.style.maxHeight;
+
+  if (maxHeight == 0) {
+    emailFormContainer.style.maxHeight = emailFormContainer.scrollHeight + 'px';
+    emailFormContainer.style.overflow = 'visible';
+  }
+  else {
+    emailFormContainer.style.maxHeight = null
+    emailFormContainer.style.overflow = 'hidden';
+  }
+}
